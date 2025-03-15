afd7415f00ed831e0722c2ce071d6cb645a3559d
jyotsnagorle <jyotsnagorle@yelp.com>
2025-03-15 15:59:39 +0100

    prettify homepage

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,47 +1,27 @@
 // screens/HomeScreen.js
 import React from 'react';
-import { SafeAreaView, Text, Button, StyleSheet } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import {Text, Button} from 'react-native-paper';
 
 
 const HomeScreen = ({ navigation }) => {
   return (
     <SafeAreaView style={[styles.container]}>
-      <Text style={[styles.text]}>Hello!</Text>
-      <Button
-        style={styles.button}
-        title="Start Today's Session"
-        onPress={() => {
+      <Text variant="headlineLarge">Hello!</Text>
+      <Button mode="contained" onPress={() => {
           const today = new Date().toISOString().split('T')[0]; // Format: YYYY-MM-DD
           navigation.navigate('WorkoutOptions', { date: today });
-        }}
-      />
+        }}>
+        Start Today's Session
+      </Button>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-  },
-  text: {
-    fontSize: 24,
-    color: 'black',
-  },
-  button: {
-    backgroundColor: '#000', // Primary color
-    paddingVertical: 12,
-    paddingHorizontal: 20,
-    borderRadius: 8,
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
+  container: { flex: 1, padding: 20, justifyContent: 'center' },
+  input: { marginVertical: 10 },
+  card: { marginVertical: 10, padding: 10 },
 });
 
 
